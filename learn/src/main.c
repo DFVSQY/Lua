ee2ef6a323ccd08911c3657568568f44acb57e24
tzj <tzj@example.com>
2022-09-03 13:42:40 +0800

    整理出一份干净的运行环境

--- learn/src/main.c
+++ learn/src/main.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 #include "testlua.h"
-#include "testc.h"
 
 int main()
 {
-	// test_operate();
-	simple_interpreter();
-
 	printf("run finish!\n");
 	return 0;
 }
--- learn/src/testc.c
+++
@@ -1,2 +0,0 @@
-#include "testc.h"
-
