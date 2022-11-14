b82acc79966a1311df90b138d2197f9daa5ff8ea
DFVSQY <dfvsqy@qq.com>
2022-11-14 22:59:04 +0800

    Chunks

--- learn/lua/main.lua
+++ learn/lua/main.lua
@@ -1,1 +1,5 @@
 print("hello world")
+
+-- Opens the named file and executes its contents as a Lua chunk
+dofile("learn/lua/test_chunk.lua")		-- test_chunk:     1
+dofile("learn/lua/test_chunk.lua")		-- test_chunk:     2
---
+++ learn/lua/test_chunk.lua
@@ -0,0 +1,4 @@
+if not _G.test_chunk then _G.test_chunk = 0 end
+
+_G.test_chunk = _G.test_chunk + 1
+print("test_chunk:", _G.test_chunk)
