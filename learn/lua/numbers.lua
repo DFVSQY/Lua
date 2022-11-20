cc67f80a4faa32a8dcaadd03f02fbb40b1a808f8
DFVSQY <dfvsqy@qq.com>
2022-11-20 21:54:47 +0800

    算数运算符

--- learn/lua/numbers.lua
+++ learn/lua/numbers.lua
@@ -68,3 +68,73 @@ print(string.format("%a", 0.5))				-- 0x1p-1
 print(string.format("%a", 10.6875))			-- 0x1.56p+3
 print(string.format("%a", 2))				-- 0x1p+1
 print(string.format("%a", 0.1))				-- 0x1.999999999999ap-4
+
+--[[
+	The addition of two integers is always an integer. The same is true for subtraction, multiplication, and negation.
+	If both operands are integers, the operation gives an integer result; otherwise, the operation results in a float.
+	In case of mixed operands, Lua converts the integer one to a float before the operation.
+	Division does not follow that rule, because the division of two integers does not need to be an integer.
+	To avoid different results between division of integers and divisions of floats,
+	division always operates on floats and gives float results
+]]
+print(13 + 15)								-- 28
+print(13.0 + 15.0)							-- 28.0
+print(13.0 + 25)							-- 38.0
+print(-(3 * 6.0))							-- -18.0
+print(3.0 / 2.0)							-- 1.5
+print(3.0 / 2)								-- 1.5
+print(3 / 2)								-- 1.5
+print(math.type(4 / 2))						-- float
+
+--[[
+	For  integer  division,  Lua  5.3  introduced  a  new  operator,  called  floor  division  and  denoted  by  //.
+	As its name implies, floor division always rounds the quotient towards minus infinity, ensuring an integral result
+	for all operands. With this definition, this operation can follow the same rule of the other arithmetic operators:
+	if both operands are integers, the result is an integer; otherwise, the result is a float (with an integral value)
+]]
+print(4 // 2)								-- 2
+print(4.0 // 2)								-- 2.0
+print(1.5 // 0.5)							-- 3.0
+print(-9 // 2)								-- -5
+print(9 // -2)								-- -5
+print(-9 // -2)								-- 4
+
+--[[
+	The modulo operator:
+	a % b == a - ((a // b) * b)
+
+	If both operands are integers, the result is an integer; otherwise, the result is a float.
+
+	For integer operands, modulo has the usual meaning,
+	with the result always having the same sign as the second  argument.
+
+
+]]
+print(4 % 2)								-- 0
+print(4.0 % 2)								-- 0.0
+print(1.5 % 0.5)							-- 0.0
+print(-9 % 2)								-- 1
+print(9 % -2)								-- -1
+print(-9 % -2)								-- -1
+
+--[[
+	For real operands, modulo has some unexpected uses.
+	For instance, x - x % 0.01 is x with exactly two decimal digits,
+	and x - x % 0.001 is x with exactly three decimal digits.
+]]
+do
+	local x = math.pi
+	print(x - x % 0.01)        				-- 3.14
+	print(x - x % 0.001)       				-- 3.141
+	print(x % 0.01)							-- 0.0015926535897931
+	print(-x % -0.01)						-- -0.0015926535897931
+	print(-x % 0.01)						-- 0.0084073464102069
+	print(x % -0.01)						-- -0.0084073464102069
+end
+
+--[[
+	Lua also offers an exponentiation operator, denoted by a caret (^).
+	Like division, it always operates on floats.
+]]
+print(4 ^ 0.5)								-- 2.0
+print(8 ^ (1 / 3))							-- 2.0
