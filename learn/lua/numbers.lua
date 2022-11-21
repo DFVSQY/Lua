9131130fec8776ea6458f2877230fc6dedc51d91
DFVSQY <dfvsqy@qq.com>
2022-11-22 00:38:48 +0800

    Conversions

--- learn/lua/numbers.lua
+++ learn/lua/numbers.lua
@@ -240,3 +240,59 @@ do
 
 	print(math.maxinteger + 1.0 ==  math.maxinteger + 2.0)		-- true (float addition)
 end
+
+--[[
+	To force a number to be a float, we can simply add 0.0 to it. An integer always can be converted to a float.
+]]
+print(-3 + 0.0)													-- -3.0
+print(math.maxinteger + 0.0)									-- 9.2233720368548e+18
+
+--[[
+	Any integer up to 253 (which is 9007199254740992) has an exact representation
+	as a double-precision floating-point number.
+	Integers with larger absolute values may lose precision when converted to a float.
+]]
+print(9007199254740991 + 0.0 == 9007199254740991)				-- true
+print(9007199254740992 + 0.0 == 9007199254740992)				-- true
+print(9007199254740993 + 0.0 == 9007199254740993)				-- false
+print(9007199254740993 + 0.0 == 9007199254740992 + 0.0)			-- true
+
+--[[
+	To force a number to be an integer, we can OR it with zero.
+
+	Lua does this kind of conversion only when the number has an exact representation as an integer,
+	that is, it has no fractional part and it is inside the range of integers.
+	Otherwise, Lua raises an error
+]]
+do
+	print(2^53)														-- 9.007199254741e+15 (always be float)
+	print(2^53 | 0)													-- 9007199254740992 (integer)
+
+	local success, err_msg
+	success, err_msg = pcall(function()
+		print(3.2 | 0)												-- fractional part
+	end)
+	print(success, err_msg)											-- false   learn/lua/numbers.lua:273: number has no integer representation
+
+	success, err_msg = pcall(function()
+		print(2 ^ 64 | 0)											-- out of range
+	end)
+	print(success, err_msg)											-- false   learn/lua/numbers.lua:278: number has no integer representation
+
+	success, err_msg = pcall(function()
+		math.random(1, 3.5)											-- fractional part
+	end)
+	print(success, err_msg)											-- false   learn/lua/numbers.lua:283: bad argument #2 to 'random' (number has no integer representation)
+end
+
+--[[
+	To round a fractional number, we must explicitly call a rounding function.
+	Another way to force a number into an integer is to use math.tointeger,
+	which returns nil when the number cannot be converted.
+]]
+do
+	print(math.tointeger(-258.0))   								-- -258
+    print(math.tointeger(2^30))   									-- 1073741824
+    print(math.tointeger(5.01))   									-- nil (not an integral value)
+    print(math.tointeger(2^64))   									-- nil (out of range)
+end
